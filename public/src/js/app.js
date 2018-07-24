e14e83e8b2c8c3f6038b4ad9d69b59de12f01a90
n41r0j <jorian@outlook.com>
2018-07-24 12:44:20 +0200

    navbar test

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var testnet = false;
-var netSymbol = testnet ? 'TAZ' : 'KMD';
+var netSymbol = testnet ? 'GLXT' : 'GLXT';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
 var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
